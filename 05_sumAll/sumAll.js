cb36a91dde95bb82cc5850510d20202b8c54adca
Manuel Pereira <130966928+manuelpereiraIII@users.noreply.github.com>
2023-05-10 13:13:12 -0700

    Sum All
    Sum All Solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(!Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0) {
+        return "ERROR";
+    }
+    
+    let sum = 0
+    const lower = Math.min(start, end);
+    const upper = Math.max(start, end);
+
+    for(let i = lower; i <= upper; i++) {
+        sum += i;
+    }
+    
+    return sum;
 
 };
 
